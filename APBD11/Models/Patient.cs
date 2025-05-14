47aaf19c4e809f5d4c0c9c9a483790ce936641d2
Michał Połosak <mpolosak@op.pl>
2025-05-14 18:39:06 +0200

    Add Models

---
+++ APBD11/Models/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APBD11.Models;
+
+public class Patient
+{
+    [Key]
+    public int IdPatient { get; set; }
+    [MaxLength(100)]
+    public string FirstName { get; set; }
+    [MaxLength(100)]
+    public string LastName { get; set; }
+    public DateTime Birthdate { get; set; }
+    
+    public ICollection<Prescription> Prescriptions { get; set; }
+}
---
+++ APBD11/Models/Prescription.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APBD11.Models;
+
+public class Prescription
+{
+    [Key]
+    public int IdPrescription { get; set; }
+    public DateOnly Date { get; set; }
+    public DateOnly DueDate { get; set; }
+    [ForeignKey(nameof(Patient))]
+    public int IdPatient { get; set; }
+    [ForeignKey(nameof(Doctor))]
+    public int IdDoctor { get; set; }
+    public Patient Patient { get; set; }
+    public Doctor Doctor { get; set; }
+    
+    public ICollection<PrescriptionMedicament> PrescriptionMedicaments { get; set; }
+}
